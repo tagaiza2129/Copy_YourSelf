767be3ac7df62897f5938837f79f18baa3d3c98e
IT-F-09 <kaneko.takumu@it-mirai-h.ibk.ed.jp>
2024-06-19 13:32:10 +0900

    Rustを使ったメインコードのためのファイルを作った(何も書いてないに等しい)

---
+++ main.rs
@@ -0,0 +1,9 @@
+use std::env;
+
+
+fn main() {
+    let _args: Vec<String> = env::args().collect();
+    print!("{}"_args)
+    // Use the args vector to process command line arguments
+    // ...
+}

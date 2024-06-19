12c4dad8aa5a5d3284087e91bde37baab7b544ff
IT-F-09 <kaneko.takumu@it-mirai-h.ibk.ed.jp>
2024-06-19 14:46:35 +0000

    管理デスクトップアプリケーションの初期設定

--- main.rs
+++
@@ -1,9 +0,0 @@
-use std::env;
-
-
-fn main() {
-    let _args: Vec<String> = env::args().collect();
-    print!("{}"_args)
-    // Use the args vector to process command line arguments
-    // ...
-}

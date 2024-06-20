2fb7d739f492b2910a021c3422c4ff5d46821b63
IT-F-09 <kaneko.takumu@it-mirai-h.ibk.ed.jp>
2024-06-20 11:33:31 +0000

    コマンドライン引数を受け取るための変数の制作

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,18 @@ fn main() {
     command_line::option_add("b".to_string(),"batch".to_string(),"100".to_string(),"バッチサイズを指定します".to_string(),"BATCH".to_string());
     command_line::option_add("l".to_string(),"Lerning_mode".to_string(), "RNN".to_string(),"学習モードを指定します".to_string(),"MODE".to_string());
     command_line::option_add("s".to_string(),"server_multi_processing".to_string(),"False".to_string(),"複数PCで学習するかをしています".to_string(),"True/False".to_string());
+    command_line::option_add("g".to_string(), "GUI_ENABLED".to_string(), "True".to_string(),"GUIかCUIで起動するかを選びます(SSH等のCUIしかない場合は無効)".to_string(),"GUI/CUI".to_string());
     let args: Vec<String> = env::args().collect();
     let _command_line_options=command_line::run_command_line(args);
+    let _device = _command_line_options.get("device").unwrap();
+    let _epoch = _command_line_options.get("epoch").unwrap();
+    let _batch = _command_line_options.get("batch").unwrap();
+    let _mode = _command_line_options.get("Lerning_mode").unwrap();
+    let _server_multi_processing = _command_line_options.get("server_multi_processing").unwrap();
+    let _gui = _command_line_options.get("GUI_ENABLED").unwrap();
     println!("{:?}",_command_line_options);
     // GUIが使えるかどうかの判定
-    if cfg!(feature = "gui")&& "GUI_ENABLED"== "True" {
+    if cfg!(feature = "gui")&& _gui== "True" {
         println!("GUI は有効です");
     } else {
         println!("GUI is disabled");

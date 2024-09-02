026e517b103f75955b9d533b4f861cc9e5752288
tagaiza2129 <tagaiza819@gmail.com>
2024-09-02 16:31:41 +0900

    flaskサーバーの構築

--- src/main.py
+++ src/main.py
@@ -2,12 +2,26 @@ from flask import Flask, render_template, request
 import os
 import yaml
 import ssl
+import random
+import string
 os.chdir("../")
 app_dir=os.getcwd()
 app = Flask(__name__)
 @app.route("/")
 def main():
     return render_template(os.path.join(app_dir,"src","index.html"))
+@app.route("/model_upload",methods=["POST"])
+def upload():
+    file_pass=[random.choice(string.ascii_letters + string.digits) for i in range(n)]
+    file_pass="".join(file_pass)
+    file=request.files["file"]
+    file_name=file.filename
+    file.save(os.path.join(app_dir,"model",file.filename))
+    os.rename(os.path.join(app_dir,"model",file.filename),os.path.join(app_dir,"model",file_name))
+    return file_pass
+@app.route("</file_pass>")
+async def file(file_pass):
+    return send_from_directory(os.path.join(app_dir,file_pass))
 if __name__ == "__main__":
     with open("config.yaml",mode="r",encoding="UTF-8")as f:
         config = yaml.safe_load(f)
@@ -18,6 +32,9 @@ if __name__ == "__main__":
     parser.add_argument("-k","--public_key",type=str,help="公開鍵を指定します",default=config["public_key_path"])
     parser.add_argument("-c","--cert",type=str,help="証明書を指定します",default=config["cert_path"])
     args = parser.parse_args()
-    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    context.load_cert_chain(args.cert, args.public_key)
-    app.run(host=args.address,port=args.port,ssl_context=context,debug=False)
+    if args.public_key =="No" or args.cert=="No":
+        app.run(host=args.address,port=args.port,debug=False)    
+    else:
+        context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+        context.load_cert_chain(args.cert, args.public_key)
+        app.run(host=args.address,port=args.port,ssl_context=context,debug=False)
